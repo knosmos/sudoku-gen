d640f4cdd5b13133f88cf8973f3472518d502dbc
unknown <knosmos@gmail.com>
2021-11-23 17:01:20 -0500

    Fully functional sudoku generation

--- gen.py
+++ gen.py
@@ -1,4 +1,7 @@
-import copy
+import copy, random
+
+def printBoard(board):
+    print("\n".join([str([board[k] for k in range(i*9,i*9+9)]) for i in range(9)]))
 
 def valid(board, cell):
     row = cell // 9
@@ -20,7 +23,7 @@ def valid(board, cell):
                 return False
     return True
 
-def solve(board):
+def solve(board, findunique = True):
     '''
     Solves board, a 1d array of len 81 containing sudoku grid (left -> right and
     top -> bottom); 0 represents blank
@@ -29,7 +32,7 @@ def solve(board):
 
     currentCell = 0
     solutions = []
-    solution = [0]*81
+    solution = copy.deepcopy(board)
     while currentCell >= 0:
         while currentCell != 81:
             if currentCell <= -1:
@@ -54,14 +57,18 @@ def solve(board):
                 else:
                     if valid(solution, currentCell):
                         currentCell += 1
+            #print("\n".join([str([solution[k] for k in range(i*9,i*9+9)]) for i in range(9)])+"\n")
         else:
             #print("\n".join([str([solution[k] for k in range(i*9,i*9+9)]) for i in range(9)]))
             #print()
             solutions.append(copy.deepcopy(solution))
-            # backtrack
-            currentCell -= 1
-            while board[currentCell] != 0:
+            if findunique and len(solutions) == 1:
+                # backtrack
                 currentCell -= 1
+                while board[currentCell] != 0:
+                    currentCell -= 1
+            else:
+                break
     #print("\n".join([str([solution[k] for k in range(i*9,i*9+9)]) for i in range(9)]))
     return solutions
 '''
@@ -79,4 +86,52 @@ solve([
     0,0,0, 0,8,0, 0,7,9
 ])'''
 
-solve([1,2,3,4,5,6,7,8,9,4,5,6,7,8,9,1,2,3]+[0]*63)
+#solve([1,2,3,4,5,6,7,8,9,4,5,6,7,8,9,1,2,3]+[0]*63)
+
+def removeSquares(n, board):
+    while True:
+        square = random.randint(0,80)
+        while board[square] == 0:
+            square = random.randint(0,80)
+        b = copy.deepcopy(board)
+        b[square] = 0
+        if len(solve(b)) >= 2:
+            continue
+        if n == 0:
+            return b
+        else:
+            return removeSquares(n-1, b)
+
+def generate(difficulty):
+    # Fill in starter squares
+    board = [0] * 81
+    rand = []
+    m = list(range(1,10))
+    for i in range(3):
+        random.shuffle(m)
+        rand.append(copy.deepcopy(m))
+    print(rand)
+    for k in range(3):
+        box = []
+        c = 0
+        for i in range(k*3, k*3 + 3):
+            for j in range(k*3, k*3 + 3):
+                board[i*9 + j] = rand[k][c]
+                c += 1
+    '''n = list(set(list(range(1,10)))-set(rand[0][:3]))
+    random.shuffle(n)
+    for i in range(6):
+        board[i+3] = n[i]'''
+    #print("scaffold:")
+    #print("\n".join([str([board[k] for k in range(i*9,i*9+9)]) for i in range(9)]))
+    print("\nfilled:")
+    board = solve(board, findunique = False)[0]
+    printBoard(board)
+
+    # remove squares one at a time
+    print("\ncomplete:")
+    final = removeSquares(difficulty, board)
+    printBoard(final)
+    return final, board # puzzle, solution
+
+generate(50)

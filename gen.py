22de06afd7c52bfd6f675ed8d9cfe259e897c0f5
unknown <knosmos@gmail.com>
2021-11-23 00:47:42 -0500

    Add functionality to generate all solution boards

--- gen.py
+++ gen.py
@@ -1,3 +1,5 @@
+import copy
+
 def valid(board, cell):
     row = cell // 9
     col = cell % 9
@@ -26,30 +28,43 @@ def solve(board):
     '''
 
     currentCell = 0
+    solutions = []
     solution = [0]*81
-    while currentCell != 81:
-        if board[currentCell] != 0:
-            solution[currentCell] = board[currentCell]
-            if valid(solution, currentCell):
-                currentCell += 1
-            else:
-                # backtrack
-                currentCell -= 1
-                while board[currentCell] != 0:
-                    currentCell -= 1
-        else:
-            solution[currentCell] += 1
-            if solution[currentCell] > 9:
-                solution[currentCell] = 0
-                # backtrack
-                currentCell -= 1
-                while board[currentCell] != 0:
-                    currentCell -= 1
-            else:
+    while currentCell >= 0:
+        while currentCell != 81:
+            if currentCell <= -1:
+                break # No more solutions
+            if board[currentCell] != 0:
+                solution[currentCell] = board[currentCell]
                 if valid(solution, currentCell):
                     currentCell += 1
-    print("\n".join([str([solution[k] for k in range(i*9,i*9+9)]) for i in range(9)]))
-
+                else:
+                    # backtrack
+                    currentCell -= 1
+                    while board[currentCell] != 0:
+                        currentCell -= 1
+            else:
+                solution[currentCell] += 1
+                if solution[currentCell] > 9:
+                    solution[currentCell] = 0
+                    # backtrack
+                    currentCell -= 1
+                    while board[currentCell] != 0:
+                        currentCell -= 1
+                else:
+                    if valid(solution, currentCell):
+                        currentCell += 1
+        else:
+            #print("\n".join([str([solution[k] for k in range(i*9,i*9+9)]) for i in range(9)]))
+            #print()
+            solutions.append(copy.deepcopy(solution))
+            # backtrack
+            currentCell -= 1
+            while board[currentCell] != 0:
+                currentCell -= 1
+    #print("\n".join([str([solution[k] for k in range(i*9,i*9+9)]) for i in range(9)]))
+    return solutions
+'''
 solve([
     5,3,0, 0,7,0, 0,0,0,
     6,0,0, 1,9,5, 0,0,0,
@@ -62,6 +77,6 @@ solve([
     0,6,0, 0,0,0, 2,8,0,
     0,0,0, 4,1,9, 0,0,5,
     0,0,0, 0,8,0, 0,7,9
-])
+])'''
 
-#print(valid([5, 3, 4, 6, 7, 8, 1, 9, 2, 6, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6, 1, 4, 2, 3, 4, 2, 6, 8, 5, 3, 9, 7, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7, 4, 1, 9, 6, 3, 5, 3, 4, 5, 2, 8, 6, 7, 7, 9], 78))
+solve([1,2,3,4,5,6,7,8,9,4,5,6,7,8,9,1,2,3]+[0]*63)
